6a58327d3605e60bcba4c8bc893dafb79f105dd2
Abhi Yerra <ykabhinav@gmail.com>
2010-02-11 09:48:48 -0800

    More base cases for the urls

--- webhound.rb
+++ webhound.rb
@@ -13,6 +13,7 @@ def crawler page
   begin
     return if $crawled.find_one({ "url" => page })
 
+
     # TODO: Minimize these.
     return if $queue_failed.include? page
 
@@ -29,45 +30,60 @@ def crawler page
     doc.search('a').each do |l|
       link = l.attributes['href'].to_s
 
-      link =~ /^(https?:\/\/)([0-9A-Za-z_\.:]+)(.*)/
+      link =~ /^(https?:\/\/)([0-9A-Za-z_\.:]+\/)(.*)/
 
       http = $1  
       uri = $2
       path = $3
+      # TODO: params
 
-      new_link = http
+      new_link = ''
+
+      # Ignore these conditions
+      next if link =~ /^#.*/ #|| link =~ // || link =~ /javascript.*/
 
       if link =~ /^https?/
         # Full url. ex. http://google.com or http://google.com/basdasd
         new_link = link
+      elsif link =~ /^\/(.*)\/$/
+        # /test/test/ - don't want t add an extra /
+        new_link = page_http + page_uri + '/' + $1
       elsif link =~ /^\/(.*)/
         # ex. /test/test
-        # TODO: Deal with cases like:
-          # /test/test/ - don't want t add an extra /
-          # /test/test?test=test - stop at ?
         new_link = page_http + page_uri + '/' + $1
+
+        # TODO: Deal with cases like basically parameters
+          # /test/test?test=test - stop at ?
+          # test - 
+      elsif link =~ /^[^\/]?(.*)/
+        #puts "here"
+        new_link = page + (page =~ /.*\/$/ ? '' : '/') + link
+        #puts new_link
+      elsif link =~ /^mailto:(.*)/
+        email = $1
+        # TODO: Do stuff with email.
       else
         new_link = page + "/" + link
       end
 
       $queue << new_link
     end
-  rescue
-    puts page
-    $queue_failed << page
-  end
+ rescue
+   puts page
+   $queue_failed << page
+ end
 end
 
 def start_crawling
   while !$queue.empty?
     page = $queue.first
     $queue.shift
-
+    
     crawler page
   end
 end
 
 start_urls = ['http://www.berkeley.edu/']
-$queue = start_url
+$queue = start_urls
 
 start_crawling
